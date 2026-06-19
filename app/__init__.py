cc5d14d6147472c46c5163eddd9e9b78a9b66b89
Chandler Xie <ChandlerX101@gmail.com>
2026-06-19 16:32:40 -0400

    added my photos with a carousel slideshow

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+photos = ["IMG_2488.jpg", "IMG_2493.jpg", "IMG_2402.jpg"]
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), photos=photos)
+
