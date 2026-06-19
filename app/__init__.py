64878a3615778e7093ba6209374b1857e34b9965
Chandler Xie <ChandlerX101@gmail.com>
2026-06-19 16:46:23 -0400

    separate hobbies page with its own route

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,19 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+hobbies = [
+    {"name": "Hiking", "description": "Love exploring trails and getting summit views."},
+    {"name": "Gaming", "description": "Currently grinding ranked in Valorant."},
+    {"name": "Cooking", "description": "My specialty is stir fry but getting into baking."}
+]
+
 
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+
+@app.route('/hobbies')
+def hobbies_page():
+    return render_template('hobbies.html', title="My Hobbies", hobbies=hobbies)
+
