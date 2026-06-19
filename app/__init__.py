beb7d35b0e29048846a16cac2dd89b46fe4c9ece
Chandler Xie <ChandlerX101@gmail.com>
2026-06-19 16:40:04 -0400

    hobbies section with images

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,26 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+hobbies = [
+    {
+        "name": "Hiking",
+        "image": "hiking.png",
+        "description": "Love getting outside and exploring trails whenever I can. Nothing beats a good summit view."
+    },
+    {
+        "name": "Gaming",
+        "image": "gaming.png",
+        "description": "Big into both competitive and chill games. Currently grinding ranked in Valorant."
+    },
+    {
+        "name": "Cooking",
+        "image": "cooking.png",
+        "description": "Always trying to learn new recipes. My specialty is stir fry but I'm getting into baking too."
+    }
+]
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), hobbies=hobbies)
+
