e54ba70402463156fdfdbf12b785051881029c6a
Chandler Xie <ChandlerX101@gmail.com>
2026-07-10 23:33:25 -0700

    Add DELETE endpoint and endpoint testing script

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,33 @@
+from peewee import MySQLDatabase
 import os
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from dotenv import load_dotenv
+import datetime 
+from peewee import *
+from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 experiences = [
     {
@@ -66,6 +90,34 @@ def portfolio_data():
         "hobbies": hobbies,
         "locations": locations
     })
+
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts':[
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+@app.route('/api/timeline_post/<int:post_id>', methods=['DELETE'])
+def delete_time_line_post(post_id):
+    try:
+        post = TimelinePost.get_by_id(post_id)
+        post.delete_instance()
+        return "Successfully deleted", 200
+    except:
+        return "Post not found", 404
 
 # Fallback route for React Router (if using client-side routing)
 @app.route('/<path:path>')
